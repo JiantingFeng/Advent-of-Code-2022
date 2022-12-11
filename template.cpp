b18f34783e8ae6b7b9790a09eee538706677463e
JiantingFeng <gianting01@gmail.com>
2022-12-11 18:48:30 +0800

    feat(n): template file for easy use

---
+++ template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  string line;
+  while (getline(cin, line)) {
+    stringstream ss(line);
+    ss >> <INPUT>;
+  }
+  return 0;
+}
